1ed188c1a4447c049dfae71045beb7404e3ba6c0
Leandro Queiroz <leandro@neospace.ai>
2025-07-05 13:42:16 -0300

    add kamehame gif

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -26,6 +26,20 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 LV_IMG_DECLARE(balloon);
 LV_IMG_DECLARE(mountain);
 LV_IMG_DECLARE(family);
+LV_IMG_DECLARE(kame1);
+LV_IMG_DECLARE(kame2);
+LV_IMG_DECLARE(kame3);
+LV_IMG_DECLARE(kame4);
+LV_IMG_DECLARE(kame5);
+LV_IMG_DECLARE(kame6);
+LV_IMG_DECLARE(kame7);
+LV_IMG_DECLARE(kame8);
+LV_IMG_DECLARE(kame9);
+LV_IMG_DECLARE(kame10);
+
+const lv_image_dsc_t *anim_imgs[] = {
+    &kame1, &kame2, &kame3, &kame4, &kame5, &kame6, &kame7, &kame8, &kame9, &kame10,
+};
 
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
@@ -116,10 +130,18 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     lv_obj_align(top, LV_ALIGN_TOP_RIGHT, 0, 0);
     lv_canvas_set_buffer(top, widget->cbuf, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
 
-    lv_obj_t *art = lv_img_create(widget->obj);
+    // lv_obj_t *art = lv_img_create(widget->obj);
     // bool random = sys_rand32_get() & 1;
     // lv_img_set_src(art, random ? &balloon : &mountain);
-    lv_img_set_src(art, &family);
+    // lv_img_set_src(art, &family);
+
+    lv_obj_t * art = lv_animimg_create(widget->obj);
+    lv_obj_center(art);
+    lv_animimg_set_src(art, (const void **) anim_imgs, 10);
+    lv_animimg_set_duration(art, 4800);
+    lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
+    lv_animimg_start(art);
+
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 
     sys_slist_append(&widgets, &widget->node);
